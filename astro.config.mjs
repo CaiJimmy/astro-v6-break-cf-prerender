44b4756a7bb262879e645f3524aeec2caac3ad71
Jimmy Cai <jimmy@cai.im>
2026-03-12 17:26:07 +0100

    note

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import FontToBuffer from "unplugin-font-to-buffer/vite";
 
 export default defineConfig({
     adapter: cloudflare({
+        /// When this is set to 'workerd' (default), `dev` server can render the server island correctly
+        /// However, with `node` server island request will return a 400 error
+        prerenderEnvironment: 'node'
     }),
     vite: {
         plugins: [FontToBuffer()],
