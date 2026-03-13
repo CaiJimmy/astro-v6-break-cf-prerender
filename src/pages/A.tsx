669125c34c3f4451411db945f06487a3e22ea750
Jimmy Cai <jimmy@cai.im>
2026-03-13 22:00:47 +0100

    Simplify

--- src/pages/A.tsx
+++ src/pages/A.tsx
@@ -1,7 +1,7 @@
 export default function A({ content }: { content?: string }) {
     return (
         <div>
-            <p>Encoded: {content}</p>
+            <p>Content: {content}</p>
         </div>
     );
 }
